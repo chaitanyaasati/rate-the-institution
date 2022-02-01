da4d557420ad7b1a8fa6c62f6651c27bd660ad6d
Chaitanya Asati <chaitanya.asati1@gmail.com>
2022-02-01 19:05:30 +0530

    Deleted boilerplate code in App.js and App.css

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I am here in the same position</h1>
+      <h3>We are working on same function</h3>
     </div>
   );
 }
